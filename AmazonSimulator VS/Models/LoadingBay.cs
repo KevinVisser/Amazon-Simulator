7d526b562d26efe4549cc5af397178c4eaddbdd5
Kevin <kevin.visser.1995@gmail.com>
2018-09-15 19:05:05 +0200

    Classes juist en objecten toegevoegd

---
+++ AmazonSimulator VS/Models/LoadingBay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class LoadingBay : Model3D
+    {
+        public LoadingBay(double x, double y, double z, double rotationX, double rotationY, double rotationZ)
+        {
+            type = "loadingBay";
+            guid = Guid.NewGuid();
+
+            _x = x;
+            _y = y;
+            _z = z;
+
+            _rX = rotationX;
+            _rY = rotationY;
+            _rZ = rotationZ;
+        }
+    }
+}
---
+++ AmazonSimulator VS/Models/PalletRack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class PalletRack : Model3D
+    {
+        public PalletRack(double x, double y, double z, double rotationX, double rotationY, double rotationZ)
+        {
+            type = "palletRack";
+            guid = Guid.NewGuid();
+
+            _x = x;
+            _y = y;
+            _z = z;
+
+            _rX = rotationX;
+            _rY = rotationY;
+            _rZ = rotationZ;
+        }
+    }
+}
